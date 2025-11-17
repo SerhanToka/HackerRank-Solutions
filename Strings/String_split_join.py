33d6812312993f7c36286d2f111fd427ba8b1823
SerhanToka <serhantokaa@hotmail.com>
2025-11-17 18:44:31 +0300

    Solved 'String split and join' in Strings

---
+++ Strings/String_split_join.py
@@ -0,0 +1,11 @@
+# Given Code
+def split_and_join(line):
+    # Answer
+    line = line.split(" ")
+    line = "-".join(line)
+    return line
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
